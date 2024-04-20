41f00c4c7611b3658f1b1afe4c9679d4de91cd98
Samuel Novaes <samuel.novaes.96@gmail.com>
2024-04-20 03:32:16 -0300

    update ne function

--- lib/ne.go
+++ lib/ne.go
@@ -5,9 +5,5 @@ import (
 )
 
 func Ne(ctx *ast.Context, args ...ast.Expression) ast.Expression {
-	isNotEqual := false
-	if args[0].GetValue(ctx) != args[1].GetValue(ctx) {
-		isNotEqual = true
-	}
-	return &ast.Boolean{Value: isNotEqual}
+	return &ast.Boolean{Value: args[0].GetValue(ctx) != args[1].GetValue(ctx)}
 }
