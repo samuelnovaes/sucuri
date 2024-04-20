2801272da8cdf46128d5629fff9796b7761cb7e8
Samuel Novaes <samuel.novaes.96@gmail.com>
2024-04-20 03:31:33 -0300

    update eq function

--- lib/eq.go
+++ lib/eq.go
@@ -5,9 +5,5 @@ import (
 )
 
 func Eq(ctx *ast.Context, args ...ast.Expression) ast.Expression {
-	isEqual := false
-	if args[0].GetValue(ctx) == args[1].GetValue(ctx) {
-		isEqual = true
-	}
-	return &ast.Boolean{Value: isEqual}
+	return &ast.Boolean{Value: args[0].GetValue(ctx) == args[1].GetValue(ctx)}
 }
