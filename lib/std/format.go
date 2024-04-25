2648cf92815a0a756342f8dd2f05e8c937b7d431
Samuel Novaes <samuel.novaes.96@gmail.com>
2024-04-25 00:19:39 -0300

    update format

--- lib/std/format.go
+++ lib/std/format.go
@@ -2,12 +2,14 @@ package std
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/samuelnovaes/sucuri/ast"
 )
 
 func Format(ctx *ast.Context, args ...ast.Expression) ast.Expression {
 	format := args[0].(*ast.String).Value
+	format = strings.ReplaceAll(format, "{}", "%v")
 	strs := []any{}
 	for _, arg := range args[1:] {
 		strs = append(strs, arg.GetValue(ctx))
